6795890c183a7960eaba140584dae27deae32f4e
devori <devori.lee@gmail.com>
2026-02-10 23:09:13 +0900

    SEO: SITE_URL을 blog.devori.app으로 변경

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,5 @@
 export const SITE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL ||
-  (process.env.VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : "http://localhost:3000");
+  process.env.NEXT_PUBLIC_SITE_URL || "https://blog.devori.app";
 
 export const SITE_NAME = "Leo의 블로그";
 export const SITE_DESCRIPTION = "생각과 경험을 기록하는 공간. 개발, 디자인, 그리고 일상에 대한 이야기.";
